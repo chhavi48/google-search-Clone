2ebd3fae81078b7c938ed73ce78f53707f23d9a9
Chhavi srivastav <77965216+chhavi48@users.noreply.github.com>
2023-01-08 00:00:25 +0530

    basic stucture

--- src/components/HomeHeader.jsx
+++ src/components/HomeHeader.jsx
@@ -1,7 +1,23 @@
 import ProfileIcon from "./ProfileIcon";
 
 const HomeHeader = () => {
-    return <div>Home header</div>;
+    return (
+        <header className="h-16 flex justify-between md:justify-end items-center gap-4 px-5">
+            <div className="flex gap-4">
+                <span className="text-black/[0.87]
+                 text-[13px] line-height hover:underline
+                  cursor-pointer">
+                    Gmail
+                </span>
+                <span className="text-black/[0.87] text-[13px]
+                 line-height 
+                 hover:underline cursor-pointer">
+                    Image
+                </span>
+            </div>
+            <ProfileIcon/>
+        </header>
+    );
 };
 
 export default HomeHeader;
--- src/components/ProfileIcon.jsx
+++ src/components/ProfileIcon.jsx
@@ -4,7 +4,14 @@ import Profile from "../assets/profile-200x200.jpg";
 import ProfileRing from "../assets/profile-ring.svg";
 
 const ProfileIcon = () => {
-    return <div>Profile Icon</div>;
+    return <div className="flex gap-2">
+            <span className="h-10 w-10 items-center justify-center
+             rounded-full cursor-pointer
+             hover:bg-black/[0.05]
+             ">
+                <TbGridDots size={20} color="#5f6368"></TbGridDots>
+             </span>
+    </div>;
 };
 
 export default ProfileIcon;
