42bed9a556879b0edebe232387b995b62ced6b4c
Chhavi srivastav <77965216+chhavi48@users.noreply.github.com>
2023-01-13 14:29:57 +0530

    show text data in first page

--- src/components/SearchResult.jsx
+++ src/components/SearchResult.jsx
@@ -10,10 +10,50 @@ import Pagination from "./Pagination";
 import { Context } from "../utils/ContextApi";
 
 const SearchResult = () => {
+  const [result,setresult]=useState()
+  const {query,startIndex}=useParams()
+  const {imageSearch}=useContext(Context)
+
+  useEffect(()=>{
+   fetchSearchResult()
+  },[query,startIndex,imageSearch])
+
+  const fetchSearchResult=()=>{
+    let payload={q:query,start:startIndex}
+    if(imageSearch){
+      payload.searchType="image"
+    }
+    fetchDataFromApi(payload).then((res)=>{
+      console.log(res);
+      setresult(res)
+    })
+  }
+
+if(!result) return ;
+const { items, queries, searchInformation } = result;
+
     return <div className="flex flex-col min-h-[100vh]">
   <SearchResultHeader/>
   <main className="grow p-[12px] pb-0 md:pr-5 md:pl-20">
-
+  <div className="flex text-sm text-[#70757a] mb-3">
+    {`About ${searchInformation.formattedTotalResults} results in (${searchInformation.formattedSearchTime})`}
+  </div>
+  {!imageSearch ? (
+                    <>
+                        {items.map((item, index) => (
+                            <SearchedItemTemplate key={index} data={item} />
+                        ))}
+                    </>
+                ) : (
+                    <div className="grid grid-cols-2 md:grid-cols-4 xl:grid-cols-6 gap-4">
+                        {items.map((item, index) => (
+                            <SearchedImageItemTemplate
+                                key={index}
+                                data={item}
+                            />
+                        ))}
+                    </div>
+                )}
   
   </main>
   <Footer/>
